239957b0c2e126fad3b86924ce5117cae8db93cb
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2011-12-24 00:09:19 -0500

    added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beam::Application.config.secret_token = '9ebd2ea2c20573a531d8f75f4b073e84018022790f7770fbe04cf44f45afd11680fd3a34789de03a05a86a59da5e8d04c1f28fa5d0219ca84b703c7db22505fc'
